3d6db435eb52d20c17c0ce561fdecfa618b60b47
Dilan1234321 <127708683+Dilan1234321@users.noreply.github.com>
2025-04-14 14:48:59 -0400

    Reverse Changes
    My bad, couldn't figure out how to run the simulation on MacOS. So I reversed all my changes.
    
    - I kept Debug to TRUE so we can debug

--- bomb_configs.py
+++ bomb_configs.py
@@ -6,9 +6,9 @@
 
 # constants
 DEBUG = True        # debug mode?
-RPi = False           # is this running on the RPi?
+RPi = True           # is this running on the RPi?
 ANIMATE = True       # animate the LCD text?
-SHOW_BUTTONS = True # show the Pause and Quit buttons on the main LCD GUI?
+SHOW_BUTTONS = False # show the Pause and Quit buttons on the main LCD GUI?
 COUNTDOWN = 300      # the initial bomb countdown value (seconds)
 NUM_STRIKES = 5      # the total strikes allowed before the bomb "explodes"
 NUM_PHASES = 4       # the total number of initial active bomb phases
