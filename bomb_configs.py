a1311c26cab76edd6277f9db6c0d9a63ccf21dee
Dilan1234321 <127708683+Dilan1234321@users.noreply.github.com>
2025-04-14 13:41:33 -0400

    Changes
    I made Rpi false, debug true, and show_buttons true because I don't have the bomb hardware so Im doing this so I can run the game on my Mac.

--- bomb_configs.py
+++ bomb_configs.py
@@ -5,10 +5,10 @@
 #################################
 
 # constants
-DEBUG = False        # debug mode?
-RPi = True           # is this running on the RPi?
+DEBUG = True        # debug mode?
+RPi = False           # is this running on the RPi?
 ANIMATE = True       # animate the LCD text?
-SHOW_BUTTONS = False # show the Pause and Quit buttons on the main LCD GUI?
+SHOW_BUTTONS = True # show the Pause and Quit buttons on the main LCD GUI?
 COUNTDOWN = 300      # the initial bomb countdown value (seconds)
 NUM_STRIKES = 5      # the total strikes allowed before the bomb "explodes"
 NUM_PHASES = 4       # the total number of initial active bomb phases
